389e922a0586aea26d4102ba726f143f6fff912a
kennedyEmanoel <79938217+kennedyEmanoel@users.noreply.github.com>
2021-09-15 09:38:26 -0300

    add bank functions

--- myBank/ContaCorrente.cs
+++ myBank/ContaCorrente.cs
@@ -14,4 +14,36 @@ public class ContaCorrente
         this.conta = conta;
         this.saldo = saldo;
     }
+
+    public bool Sacar (double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar (double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir (double valor, ContaCorrente contaDestino)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.Sacar(valor);
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
 }
--- myBank/Program.cs
+++ myBank/Program.cs
@@ -6,12 +6,20 @@ namespace myBank
     {
         static void Main(string[] args)
         {
+            ContaCorrente TioPatinhas = new ContaCorrente("Tio Patinhas", 1111, 2222, 343179960000);
             ContaCorrente ContaDoKennedy = new ContaCorrente("Kennedy", 1234, 798, 11);
-
-            ContaCorrente ContaDoJoao = new ContaCorrente("João", 1235, 758, 11);
+            ContaCorrente ContaDoJoao = new ContaCorrente("João", 1235, 758, 1901);
         
             Console.WriteLine("O Titular da conta é: " + ContaDoKennedy.titular + " || saldo: " + ContaDoKennedy.saldo);
-            Console.WriteLine("O saldo da conta é: " + ContaDoKennedy.saldo + " || saldo: " + ContaDoJoao.saldo);
+
+            bool retorno = ContaDoKennedy.Sacar(10);
+            Console.WriteLine("O saldo da conta do Kennedy é: " + ContaDoKennedy.saldo);
+
+            Console.WriteLine("O saldo da conta do Tio Patinhas é de: " + TioPatinhas.saldo);
+
+            TioPatinhas.Transferir(2000, ContaDoKennedy);
+            Console.WriteLine("O saldo da conta do Kennedy é de: " + ContaDoKennedy.saldo);
+            Console.WriteLine("O saldo da conta do Tio Patinhas é de: " + TioPatinhas.saldo);
         }
     }
 }
